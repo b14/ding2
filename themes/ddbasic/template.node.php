f14477ea4e5531719adbbbbaae4f8dd6ad47b9b7
steen <steen@b14.dk>
2017-12-08 09:24:22 +0100

    3084: Fixed elf in search/replace

--- themes/ddbasic/template.node.php
+++ themes/ddbasic/template.node.php
@@ -373,7 +373,7 @@ function ddbasic_preprocess__node__ding_eresource(&$variables) {
 function ddbasic_preprocess__node__ding_page(&$variables) {
   switch ($variables['view_mode']) {
     case 'teaser':
-      if( !empty($variables['field_ding_page_list_image'][0]['uri'])) {
+      if (!empty($variables['field_ding_page_list_image'][0]['uri'])) {
         $variables['background_image'] = image_style_url('ding_list_square', $variables['field_ding_page_list_image'][0]['uri']);
       } else {
         $variables['background_image'] = '';
@@ -382,7 +382,7 @@ function ddbasic_preprocess__node__ding_page(&$variables) {
     case 'teaser_no_overlay':
       array_push($variables['classes_array'], 'node-teaser-no-overlay');
 
-      if( !empty($variables['field_ding_page_list_image'][0]['uri'])) {
+      if (!empty($variables['field_ding_page_list_image'][0]['uri'])) {
         $variables['background_image'] = image_style_url('ding_list_square', $variables['field_ding_page_list_image'][0]['uri']);
       } else {
         $variables['background_image'] = '';
